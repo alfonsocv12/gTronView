42dda7c98de96a892db116a62a07a24b3a22893b
alfonsocv12 <alfonsocvu@gmail.com>
2022-02-01 18:37:24 -0700

    Remove comments

--- index.js
+++ index.js
@@ -12,17 +12,11 @@ app.on("ready", () => {
     mainWindow = window.createBrowserWindow(app);
     remoteMain.enable(mainWindow.webContents);
 
-    // Option 1: Uses Webtag and load a custom html file with external content
-    // mainWindow.loadURL(`file://${__dirname}/index.html`);
-
     // Option 2: Load directly an URL if you don't need interface customization
     mainWindow.loadURL("https://mail.google.com/chat/u/0/#chat/welcome");
     mainWindow.webContents.on('did-finish-load', () => {
         mainWindow.setTitle('Gtron view');
     })
-    // Option 3: Uses BrowserView to load an URL
-    //const view = require("./src/view");
-    //view.createBrowserView(mainWindow);
 });
 
 // Quit when all windows are closed.
