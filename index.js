12d26c43071bc1657a0836a7094e66a1fa8cc6b6
alfonsocv12 <alfonsocvu@gmail.com>
2022-02-02 20:06:08 -0700

    Continue with notification badge and header

--- index.js
+++ index.js
@@ -3,10 +3,24 @@ const { app, Menu } = require("electron");
 const remoteMain = require("@electron/remote/main");
 remoteMain.initialize();
 
+// const dockMenu = Menu.buildFromTemplate([
+//     {
+//         label: 'New Window',
+//         click() { console.log('New Window') }
+//     }, {
+//         label: 'New Window with Settings',
+//         submenu: [
+//             { label: 'Basic' },
+//             { label: 'Pro' }
+//         ]
+//     },
+//     { label: 'New Command...' }
+// ])
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
-app.allowRendererProcessReuse = true;
-app.on("ready", () => {
+// app.allowRendererProcessReuse = true;
+app.on("ready", (event, launchInfo) => {
     // Main window
     const window = require("./src/window");
     mainWindow = window.createBrowserWindow(app);
@@ -17,9 +31,14 @@ app.on("ready", () => {
     mainWindow.webContents.on('did-finish-load', () => {
         mainWindow.setTitle('gcview');
     })
+
+    app.on('before-quit', () => {
+        console.log(launchInfo);
+    })
 });
 
 // Quit when all windows are closed.
 app.on("window-all-closed", () => {
+    console.log('here');
     app.quit();
 });
