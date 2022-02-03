aa1dfb8f9072c39da8f5d5be88c0af0c8167c084
alfonsocv12 <alfonsocvu@gmail.com>
2022-02-02 21:12:07 -0700

    Notification badge working

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Electron
-const { app, Menu } = require("electron");
+const { app, Menu, ipcMain } = require("electron");
 const remoteMain = require("@electron/remote/main");
 remoteMain.initialize();
 
@@ -17,6 +17,15 @@ remoteMain.initialize();
 //     { label: 'New Command...' }
 // ])
 
+
+ipcMain.on('change-badge', (event, data) => {
+    if (data !== '0' && data !== '') {
+        app.dock.setBadge(data);
+    } else {
+        app.dock.setBadge('');
+    }
+})
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // app.allowRendererProcessReuse = true;
@@ -26,14 +35,33 @@ app.on("ready", (event, launchInfo) => {
     mainWindow = window.createBrowserWindow(app);
     remoteMain.enable(mainWindow.webContents);
 
+    app.dock.setBadge('');
+
     // Option 2: Load directly an URL if you don't need interface customization
     mainWindow.loadURL("https://mail.google.com/chat/u/0/#chat/welcome");
     mainWindow.webContents.on('did-finish-load', () => {
         mainWindow.setTitle('gcview');
-    })
 
-    app.on('before-quit', () => {
-        console.log(launchInfo);
+        mainWindow.webContents.executeJavaScript(`
+            const notificationSpan = document.querySelector('.XU');
+
+            window.api.topic.ipcRenderer.send(
+                'change-badge',
+                notificationSpan.innerText
+            );
+
+            notificationSpan.addEventListener('DOMSubtreeModified', () => {
+                window.api.topic.ipcRenderer.send(
+                    'change-badge',
+                    document.querySelector('.XU').innerText
+                );  
+            })
+        `);
+
+        mainWindow.webContents.on('found-in-page', (event, result) => {
+            console.log(event);
+            console.log(result);
+        })
     })
 });
 
--- preload.js
+++ preload.js
@@ -1,5 +1,18 @@
 const { BrowserWindow } = require("@electron/remote");
+const { contextBridge } = require('electron');
+// const { hello } = require('./src/sendDomData');
+
+contextBridge.exposeInMainWorld(
+    'api',
+    {
+        platform: process.platform,
+        topic: {
+            ipcRenderer: require('electron').ipcRenderer
+        }
+    }
+)
 
 window.addEventListener("DOMContentLoaded", () => {
     // Add Functions
+    window.ipcRenderer = require('electron').ipcRenderer
 });
